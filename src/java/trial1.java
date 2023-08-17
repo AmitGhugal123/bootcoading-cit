796bfa563208b35e777df579fe7b2609eeb90d93
Dell <gn2618pbn1@gmail.com>
2023-08-17 15:55:11 +0530

    ......

---
+++ src/java/trial1.java
@@ -0,0 +1,12 @@
+public class trial1 {
+    public static void  main(String[] args) {
+        int hours = 10;
+        int minutes = 2;
+        int seconds = minutes * 60;
+        int hourSeconds = hours * 60 * 60;
+
+        System.out.println("hour to seconds: " + hourSeconds);
+        System.out.println("Minute to seconds : " + seconds);
+
+    }
+}
